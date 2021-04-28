9d80313ff63191d01337057f7cca7599c175c0f8
mohammadrad81 <mohammad.rad81@gmail.com>
2021-04-28 17:06:45 +0430

    just a push

--- src/ConsoleColors.java
+++ src/ConsoleColors.java
@@ -16,6 +16,17 @@ public class ConsoleColors {
 //    public static final String CYAN = "\033[0;36m";    // CYAN
 //    public static final String WHITE = "\033[0;37m";   // WHITE
 
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
+
 //    public static final String RESET = "\u001B[0m";
 //    public static final String BLACK= "\u001B[0m";
 //    public static final String WHITE = "\u001B[0m";
@@ -26,20 +37,8 @@ public class ConsoleColors {
 //    public static final String BLUE = "\u001B[34m";
 //    public static final String PURPLE = "\u001B[35m";
 //    public static final String CYAN = "\u001B[36m";
-
-    public static final String RESET = "\u001B[0m";
-    public static final String BLACK= "\033[0;37m";
-    public static final String WHITE = "\033[0;37m";
-    //    public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-
-//    public static final String WHITE = "\u001B[37m"; // IT'S INSTEAD BLACK
-//    public static final String BLACK = "\u001B[37m"; // IT'S INSTEAD BLACK
+////    public static final String WHITE = "\u001B[37m"; // IT'S INSTEAD BLACK
+////    public static final String BLACK = "\u001B[37m"; // IT'S INSTEAD BLACK
 
     // Bold
 //    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
