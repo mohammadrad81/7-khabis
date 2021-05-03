31264d723e6c7abfb19174900304c6892c6f9abe
mohammadrad81 <mohammad.rad81@gmail.com>
2021-05-03 13:53:57 +0430

    removing class files

--- src/ActionNames.java
+++
@@ -1,9 +0,0 @@
-public class ActionNames {
-    public static final String BLACK_SEVEN = "next player picks up 4 more cards";
-    public static final String REGULAR_SEVEN = "next player picks up 2 more cards";
-    public static final String TWO = "give one card of your own to another player";
-    public static final String EIGHT = "play one more card";
-    public static final String TEN = "change direction";
-    public static final String A = "jump";
-    public static final String B = "choose next card color";
-}
